16396b89b845650265fdb03a62cc13003d296bfb
Antonio Salcedo <antony9409@gmail.com>
2021-02-03 12:53:00 -0500

    Updates

---
+++ precache-manifest.34717b4b872c92a4c61468149aa392ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7f2b308930ec0511f627e4a631a9ab4",
+    "url": "/front-grid/index.html"
+  },
+  {
+    "revision": "c7124d93370a685d6a6b",
+    "url": "/front-grid/static/css/2.87a9c7a2.chunk.css"
+  },
+  {
+    "revision": "5ca1a5adcdefc619b84a",
+    "url": "/front-grid/static/css/main.f5b86faf.chunk.css"
+  },
+  {
+    "revision": "c7124d93370a685d6a6b",
+    "url": "/front-grid/static/js/2.7088475c.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/front-grid/static/js/2.7088475c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ca1a5adcdefc619b84a",
+    "url": "/front-grid/static/js/main.1a60da55.chunk.js"
+  },
+  {
+    "revision": "204c8947e38ed9e51ec4",
+    "url": "/front-grid/static/js/runtime-main.63925e6f.js"
+  }
+]);
